155b7bee3932a159b5f92f060a67e78e27407863
Gustavox2019 <egustavoalfredo1@gmail.com>
2026-05-20 23:07:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import joblib
 import numpy as np
+
 modelo = joblib.load('modelos/modelo_rf.pkl')
+
 st.title('Aplicación Machine Learning - Online Retail Dataset')
-st.write('Nombre: TU NOMBRE COMPLETO')
-st.write('Código ISIL: TU CÓDIGO ISIL')
-st.markdown('[Ver Google Colab](PEGAR_LINK_COLAB_AQUI)')
+
+st.write('Nombre: TU NOMBRE')
+st.write('Código ISIL: TU CODIGO')
+
 cantidad = st.number_input('Cantidad')
 precio = st.number_input('Precio Unitario')
-pais = st.number_input('Código País')
+
 if st.button('Predecir'):
-datos = np.array([[cantidad, precio, pais]])
-prediccion = modelo.predict(datos)
-st.write('Resultado:', prediccion)
+    datos = np.array([[cantidad, precio]])
+    prediccion = modelo.predict(datos)
+    st.write('Resultado:', prediccion)
